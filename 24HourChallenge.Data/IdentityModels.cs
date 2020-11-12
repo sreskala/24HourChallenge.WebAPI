b1f3cf9adabd7ad1b223cbccd6202549832071b2
sreskala <sam.sagas@hotmail.com>
2020-11-12 14:31:34 -0500

    switching branches

--- 24HourChallenge.Data/IdentityModels.cs
+++ 24HourChallenge.Data/IdentityModels.cs
@@ -1,10 +1,14 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using _24HourChallenge.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
-namespace _24HourChallenge.WebAPI.Data
+namespace ElevenNote.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -24,10 +28,37 @@ namespace _24HourChallenge.WebAPI.Data
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new IdentityUserLoginConfiguration()).Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
---
+++ 24HourChallenge.Data/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public Guid Author { get; set; }
+
+        [Required]
+        [MaxLength(40, ErrorMessage = "Title post cannot be greater than 40 characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(8000)]
+        [MinLength(5, ErrorMessage = "Post body must be at least 5 characters")]
+        public string Text { get; set; }
+
+        //Foreign Key
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
