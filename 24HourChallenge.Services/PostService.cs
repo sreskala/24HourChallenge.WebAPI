5fa175db74c0b31b63559d05f68da89f0d09bae4
Dave Sprinkle <davesprinkle3@gmail.com>
2020-11-12 14:17:08 -0500

    Services, Part I

---
+++ 24HourChallenge.Services/PostService.cs
@@ -0,0 +1,65 @@
+﻿using _24HourChallenge.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Services
+{
+    public class PostService
+    {
+
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    Id = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Post.Add(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
+        public IEnumerable<PostListItem> GetNotes()
+        {
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Post
+                        .Where(e => e.UserId == _userId)
+                        .Select(
+                            e =>
+                                new PostListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+    }
+}
