30cfe7853d7a7dde53ac09a875d29d8722bfad75
Dave Sprinkle <davesprinkle3@gmail.com>
2020-11-12 16:19:35 -0500

    More Models Added--Comment and Reply

---
+++ 24HourChallenge.Models/CommentListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Models
+{
+    public class CommentListItem
+    {
+            public int PostId { get; set; }
+            [ForeignKey(nameof(PostId))]
+            public virtual PostListItem Post { get; set; }
+
+            public Guid Author { get; set; }
+
+            public string Text { get; set; }
+
+            public virtual List<CreateReply> Replies { get; set; }
+
+    }
+}
